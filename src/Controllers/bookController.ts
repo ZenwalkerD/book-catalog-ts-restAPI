0378412a2114d251e6109e8652104878ac921b7c
Zenwalker <zenwalker1985@gmail.com>
2019-02-15 16:56:39 +0530

    few more changes

--- src/Controllers/bookController.ts
+++ src/Controllers/bookController.ts
@@ -1,2 +1,26 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
+
+import Book from "./../book";
+
+export let allbooks = (req: Request, res: Response) => {
+    let books = Book.find((err: any, books: any) => {
+        SendResponse(err, res, books);
+    });
+}
+
+export let getBook = (req: Request, res: Response) => {
+    Book.findById(req.params.id, (error: any, book: any) => {
+        SendResponse(error, res, book);
+    });
+}
+
+
+function SendResponse(err: any, res: Response, books: any) {
+    if (err) {
+        res.send(err);
+    }
+    else {
+        res.send(books);
+    }
+}
 
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import * as bookController from "./Controllers/bookController";
+import * as bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 app.set("port", 3000);
-app.get("/",(req: any, res: any)=>{
-res.send("world world");
-});
+app.get("/books", bookController.allbooks);
+app.get("/books/:id", bookController.getBook);
 
 app.listen(app.get("port"),() => {
     console.log("app is running.." ,app.get("port"))
